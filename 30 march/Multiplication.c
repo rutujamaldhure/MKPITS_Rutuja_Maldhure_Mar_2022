aecc744eb865f903b2d36990d31c6db2669baf93
rutuja maldhure <rutujamaldhure@gmail.com>
2022-03-31 17:28:02 +0530

    multiplication program

---
+++ 30 march/Multiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1, num2, res;
+clrscr();
+printf("Enter two numbers to multiply");
+scanf("%d,%d",&num1,&num2);
+res = num1*num2;
+printf("multiplication of two number is %d",res); 
+getch();
+}
