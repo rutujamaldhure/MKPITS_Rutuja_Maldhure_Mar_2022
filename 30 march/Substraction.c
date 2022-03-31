d9a3b96be7792429bf69b9858f1e13cb71efb4ad
rutuja maldhure <rutujamaldhure@gmail.com>
2022-03-31 17:21:24 +0530

    substraction program

---
+++ 30 march/Substraction.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1, num2, res;
+printf("Enter two numbers to substract");
+scanf("%d,%d",&num1,&num2);
+res = num1-num2;
+printf("substraction of two number is %d",res); 
+getch();
+}
