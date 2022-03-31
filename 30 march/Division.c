3ee152109ea24bba9f3a192742424df8665f9c2a
rutuja maldhure <rutujamaldhure@gmail.com>
2022-03-31 17:54:06 +0530

    division program

---
+++ 30 march/Division.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1, num2;
+float res;
+printf("Enter first number ");
+scanf("%d",&num1);
+printf("\n Enter 2nd number ");
+scanf("%d",&num2);
+res = num1/num2;
+printf("division of two number is %f", res); 
+getch();
+}
