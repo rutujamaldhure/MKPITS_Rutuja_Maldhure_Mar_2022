38316c450215d1c877ad06eb4b0823b5bfb21b11
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-01 13:15:24 +0530

    program 6

---
+++ 31 march/assignment_1/06_speed.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float d, t, s;
+printf("Enter distance covered in km ");
+scanf("%f",&d);
+printf("Enter time taken in Hr ");
+scanf("%f",&t);
+s = d / t;
+printf("speed is %f km/hr", s);
+getch();
+} 
+
