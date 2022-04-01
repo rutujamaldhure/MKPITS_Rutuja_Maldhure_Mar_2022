b2234506679942bfa2a4071efe058c4613a19a7b
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-01 11:46:56 +0530

    program 2

---
+++ 31 march/assignment_1/02_area_of_square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int side, area;
+printf("Enter side of square in cm ");
+scanf("%d", &side);
+area= side*side;
+printf("Area of Square is %d sqcm",area);
+getch();
+}
