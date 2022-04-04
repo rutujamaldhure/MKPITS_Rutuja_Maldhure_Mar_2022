85d602b918387d66992be4d565275532282382f5
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-04 17:06:58 +0530

    addition of digit in 3 digit number

---
+++ C Programs/28_addition_of_digit_in_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num, n1 ,n2, n3, add;
+printf("Enter 3 digit number ");
+scanf("%d", &num);
+n1= num%10;
+num=num/10;
+n2=num%10;
+num=num/10;
+n3=num%10;
+add= n1+n2+n3;
+printf("addition of digit is %d",add);
+}
