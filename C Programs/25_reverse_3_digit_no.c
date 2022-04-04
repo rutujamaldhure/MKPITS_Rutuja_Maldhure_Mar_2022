679113ea22ed6baa986dbe77dc5999b43439c807
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-04 16:41:36 +0530

    reverse 3 digit number

---
+++ C Programs/25_reverse_3_digit_no.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num, n1 ,n2, n3, rev;
+printf("Enter 3 digit number ");
+scanf("%d", &num);
+n1= num%10;
+num=num/10;
+n2=num%10;
+num=num/10;
+n3=num%10;
+rev= n1*100+n2*10+n3;
+printf("reversed number is %d",rev);
+}
