26234312407983946a741aca45058b44078b7f36
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-05 18:03:57 +0530

    addition of two no

---
+++ C Programs/assignment 1/03_program.c
@@ -0,0 +1,13 @@
+//write a program to print the sum of two numbers.
+#include<stdio.h>
+void main ()
+{
+    int num1, num2, res;
+    printf("Enter 1st number ");
+    scanf("%d",&num1);
+    printf("Enter 2nd number ");
+    scanf("%d",&num2);
+    res= num1+num2;
+    printf("Addition of %d and %d is %d", num1,num2,res);
+
+}
