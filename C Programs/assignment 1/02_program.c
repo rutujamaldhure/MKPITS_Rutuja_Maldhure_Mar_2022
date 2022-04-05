35067ff5e0892e05be321cbdcd7e3906ec5a924a
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-05 17:54:14 +0530

    to print name, dob and mob no

---
+++ C Programs/assignment 1/02_program.c
@@ -0,0 +1,8 @@
+//write a c program to print your name, date and mobile number
+#include<stdio.h>
+void main()
+{
+    printf("Name:Rutuja Maldhure");
+    printf("\nDOB: 4 Aug, 1996");
+    printf("\nMob No.: 8412075627");
+}
