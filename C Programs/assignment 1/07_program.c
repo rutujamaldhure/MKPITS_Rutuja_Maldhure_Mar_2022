dcc3acb012a8f3b5852c1970543df694a732c3e6
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-06 12:21:11 +0530

    covert days into years weeks and days

---
+++ C Programs/assignment 1/07_program.c
@@ -0,0 +1,15 @@
+//write a program to convert specified days into years, weeks and days
+#include<stdio.h>
+void main()
+{
+    int tdays, year, week, day,t;
+    printf("Enter days to convert ");
+    scanf("%d",&tdays);
+    year = tdays/364;
+    t = tdays%364;
+    week = t/7;
+    t = t%7;
+    day = t;
+    printf("%d days means: %d years %d weeks and %d days",tdays,year,week,day);
+    
+}
