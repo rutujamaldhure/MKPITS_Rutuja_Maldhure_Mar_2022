9147704b9411ec4a3f07fec5df8ed473e078de14
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-06 22:39:40 +0530

    menu driven program02

---
+++ C Programs/31_menu_driven_program2.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+    int num1, num2, res;
+    float num3, num4 , div;
+    char choice;
+    printf("press + for addition");
+    printf("\nnpress - for substraction");
+    printf("\npress * for multiplication");
+    printf("\npress / for division");
+    printf("\nEnter your choice- ");
+    scanf("%c", &choice);
+    if(choice == '+')
+    {
+        printf("Enter two numbers to add\n");
+        printf("\n1st number: ");
+        scanf("%d",&num1);
+        printf("\n2nd number: ");
+        scanf("%d",&num2);
+        res = num1+num2;
+        printf("addition of two number is %d",res); 
+    }
+    else
+    if(choice=='-')
+    {
+        
+        printf("Enter two numbers to subtract\n");
+        printf("\n1st number: ");
+        scanf("%d",&num1);
+        printf("\n2nd number: ");
+        scanf("%d",&num2);
+        res = num1-num2;
+        printf("substraction of two number is %d",res);
+    }
+    else
+    if(choice=='*')
+    {
+        
+        printf("Enter two numbers to multiplication\n");
+        printf("\n1st number: ");
+        scanf("%d",&num1);
+        printf("\n2nd number: ");
+        scanf("%d",&num2);
+        res = num1*num2;
+        printf("multiplication of two number is %d",res);
+    }
+    else
+    if(choice=='/')
+    {
+        
+        printf("Enter two numbers to divide\n");
+        printf("\n1st number: ");
+        scanf("%f",&num3);
+        printf("\n2nd number: ");
+        scanf("%f",&num4);
+        div = num3/num4;
+        printf("addition of two number is %f",div);
+    }
+    else
+    {
+        printf("invalid choice");
+    }
+
+}
