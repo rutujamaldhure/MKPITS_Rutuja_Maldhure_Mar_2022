e0b487f476a8fddfbae59e61557f7ddb376b6747
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-06 12:47:47 +0530

    print f and c using #

---
+++ C Programs/assignment 1/09_program.c
@@ -0,0 +1,22 @@
+//write a program  to print block F using #, where the F has a height of 6 characters and width of five and four characters and also to print big c
+#include<stdio.h>
+void main()
+{
+    printf("######");
+    printf("\n#");
+    printf("\n#");
+    printf("\n#####");
+    printf("\n#");
+    printf("\n#");
+    printf("\n#");
+
+    printf("\n\n######");
+    printf("\n#    #");
+    printf("\n#");
+    printf("\n#");
+    printf("\n#");
+    printf("\n#");
+    printf("\n#    #");
+    printf("\n######");
+    
+}
