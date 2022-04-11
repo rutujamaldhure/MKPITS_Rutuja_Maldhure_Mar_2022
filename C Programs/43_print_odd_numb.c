0eb6f6dd8c07fdb5154a5ce7dbee426855024e08
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-11 17:32:07 +0530

    print odd numbers using while

---
+++ C Programs/43_print_odd_numb.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    system("cls");
+    i=1;
+    while(i<=10)
+    {
+        if(i%2!=0)
+        {
+            printf("%d\n",i);
+        }
+        i++;
+    }
+}
