409aa5d206bcb364215a250598bd5f1cb8b13612
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-12 21:34:51 +0530

    to find entred n digit number is palindrome or not

---
+++ C Programs/60_find_entred_n_digit_num_is_palindron_or_not.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main ()
+{
+    int num, backup, rev=0,n;
+    system("cls");
+    printf("Enter a number ");
+    scanf("%d",&num);
+    backup=num;
+    while(num>0)
+    {
+        n=num%10;
+        rev=rev*10+n;
+        num=num/10;
+    }
+    if (backup==rev)
+    {
+        printf("%d is palindrome ",backup);
+    }
+    else
+    {
+        printf("%d is NOT palindrome",backup);
+    }
+    
+}
