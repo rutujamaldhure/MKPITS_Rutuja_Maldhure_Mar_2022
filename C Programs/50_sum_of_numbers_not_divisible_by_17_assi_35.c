3aabcba441ba4a4a3c96090122b4dd429d3cd72b
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-12 11:34:07 +0530

    sum of number not divsible by 17 between given number ass 35

---
+++ C Programs/50_sum_of_numbers_not_divisible_by_17_assi_35.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int num1, num2, i, add=0;
+    system("cls");
+    printf("Enter 1st number\n");
+    scanf("%d", &num1);
+    printf("Enter 2nd number\n");
+    scanf("%d", &num2);
+    i=num1;
+    while(i<=num2)
+    {
+        if(i%17==0)
+        {
+            printf("%d\n", i);
+            add=add+i;
+        }
+        i++;
+    }
+
+    printf("Addition of all Numbers not divisible by 17 between %d and %d = %d ", num1, num2 ,add);
+}
