1156798dda34fc308059c91b697efbad5b1dce90
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-19 13:36:10 +0530

    print armstrong num upto 1000 using do while

---
+++ C Programs/77_armstrong_no_with_dowhile.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num,t, n1 ,n2, n3,arm;
+
+    system("cls");
+    printf("All Armstrong number between 1 to 1000: \n ");
+    num=001;
+    do
+    {
+
+        n1= num%10;
+        t=num/10;
+        n2=t%10;
+        t=t/10;
+        n3=t%10;
+        arm=n1*n1*n1+n2*n2*n2+n3*n3*n3;
+        if(num==arm)
+        {
+            printf("%d\n",num);
+        }
+        num=num+001;
+    }while(num<1000);
+}
