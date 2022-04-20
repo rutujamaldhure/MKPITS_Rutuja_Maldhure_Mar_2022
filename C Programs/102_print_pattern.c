128e17d4f38ea40cbef2d98890a243cb1a572dd8
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-20 17:50:53 +0530

    print  12345 in 5 lines

---
+++ C Programs/102_print_pattern.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int r,c,n,l;
+    system("cls");
+    for(r=1;r<=4;r++)
+    {
+        for(c=1;c<=5;c++)
+        {
+            printf("%d",c);
+        }
+        printf("\n");
+    }    
+}
