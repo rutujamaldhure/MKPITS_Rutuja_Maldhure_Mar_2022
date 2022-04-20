89dd91996d1cbe7d9b1403dfa3b862b65267700c
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-20 21:14:50 +0530

    print pattern assi-09

---
+++ C Programs/assignment to print pattern/09.c
@@ -0,0 +1,20 @@
+/*
+ABCD
+ABCD
+ABCD
+ABCD
+*/
+#include<stdio.h>
+void main()
+{
+    char c,r;
+    system("cls");
+    for(r='A'; r<='D';r++)
+    {
+        for(c='A'; c<='D';c++)
+        {
+            printf("%c",c);
+        }
+        printf("\n");
+    }
+}
