67a48b7cf1a0c1918b464fa0e027bae109941bc8
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-20 18:15:07 +0530

    print pattern assi-03

---
+++ C Programs/assignment to print pattern/03.c
@@ -0,0 +1,22 @@
+/*
+*****
+****
+***
+**
+*
+*/
+
+#include<stdio.h>
+void main ()
+{
+    int r, c;
+    system("cls");
+    for(r=5; r>0; r--)
+    {
+        for(c=1; c<=r; c++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
