74c48aeeff7872626feea5ad867e10c1fa526e34
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-21 20:50:58 +0530

    display num of array in reverse order

---
+++ C Programs/103_display_array_in_reverse_order.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int i,num[5];
+    system("cls");
+    printf("Enter any 5 numbers\n");
+    for(i=0; i<5; i++)
+    {
+        printf("num[%d]= ",i);
+        scanf("%d", &num[i]);
+    }
+    printf("Reverse order of entered number- ");
+    for(i=4; i>=0; i--)
+    {
+        printf("%d ",num[i]);
+    }
+}
