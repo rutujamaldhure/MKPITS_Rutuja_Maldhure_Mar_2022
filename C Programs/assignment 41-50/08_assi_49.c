e187f7ebeb3e5534152c48b322544a7c0a5f8f58
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-22 14:51:38 +0530

    assi-49

---
+++ C Programs/assignment 41-50/08_assi_49.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    double p,r,t,si;
+    system("cls");
+    printf("Enter principle amount ");
+    scanf("%lf",&p);
+    printf("Enter duration in year ");
+    scanf("%lf",&t);
+    printf("Enter rate of intrest per year ");
+    scanf("%lf",&r);
+    si=(p*t*r)/100;
+    printf("simple intrest= %lf",si);
+
+
+}
