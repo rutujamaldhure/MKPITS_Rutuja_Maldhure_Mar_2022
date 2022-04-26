30f87f303f46419769d10b1d655074f1866595b2
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-26 13:32:43 +0530

    remove negative sign of an integer

---
+++ C Programs/assignment 61-70/04_remove_negative_sign_assi_64.c
@@ -0,0 +1,11 @@
+//assignment 64
+#include<stdio.h>
+void main()
+{
+    int num;
+    system("cls");
+    printf("Enter negative integer: ");
+    scanf("%d",&num);
+    num=-(num);
+    printf("After removing negative sign: %d",num);
+}
