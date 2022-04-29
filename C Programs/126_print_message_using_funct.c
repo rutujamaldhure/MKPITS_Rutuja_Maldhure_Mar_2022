df388a67b7aa22323d473764c9079c38f3ea5fa2
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-29 12:31:31 +0530

    to call user defined function using for loop

---
+++ C Programs/126_print_message_using_funct.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void disname();
+void main()
+{
+    int i;
+    system("cls");
+    for(i=0; i<=10; i++)
+    {
+        disname();
+    }
+}
+void disname()
+{
+    printf("Name ");
+}
