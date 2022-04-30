4306a2432aeebdfde164e2aec3a0e8793d665b88
rutuja maldhure <rutujamaldhure@gmail.com>
2022-04-30 15:24:07 +0530

    convert str in upper case without using functn

---
+++ C Programs/133_convert_in_upper_case_without_functn.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    char input_str[20], upr_str[20];
+    int i, length=0;
+    system("cls");
+    printf("Enter string: ");
+    scanf("%s",input_str);
+    for(i=0; input_str[i]!='\0'; i++)
+    {
+        length=length+1;
+    }
+
+    for(i=0; i<length; i++)
+    {
+        if(input_str[i]>=97 && input_str[i]<=122)
+        upr_str[i]=input_str[i]-32;
+        else
+        upr_str[i]=input_str[i];
+        printf("%c",upr_str[i]);
+    }
+}
