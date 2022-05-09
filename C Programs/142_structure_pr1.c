2c0ce0ba8831269d1958f8f8d22f12e93fc9f0d5
rutuja maldhure <rutujamaldhure@gmail.com>
2022-05-09 23:13:55 +0530

    structure pr1

---
+++ C Programs/142_structure_pr1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+struct student
+{
+    int id;
+    char Name[20];
+}s1;
+void main ()
+{
+    system("cls");
+    printf("Enter student id: ");
+    scanf("%d",&s1.id);
+    printf("Enter student Name: ");
+    scanf("%s",&s1.Name);
+    printf("student id= %d",s1.id);
+    printf("\nstudent name= %s",s1.Name);
+}
