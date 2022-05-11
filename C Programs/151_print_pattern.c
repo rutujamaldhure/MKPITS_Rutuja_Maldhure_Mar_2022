79bb08900fe0796a28a19f9884f5268eb34a5268
rutuja maldhure <rutujamaldhure@gmail.com>
2022-05-11 16:11:22 +0530

    print pattern 1

---
+++ C Programs/151_print_pattern.c
@@ -0,0 +1,19 @@
+/*EDCBA
+EDCBA
+EDCBA
+EDCBA*/
+
+#include<stdio.h>
+void main()
+{
+    char i,j;
+    system("cls");
+    for(i=1; i<=4; i++)
+    {
+        for(j='E'; j>='A'; j--)
+        {
+            printf("%c",j);
+        }
+        printf("\n");
+    }
+}
