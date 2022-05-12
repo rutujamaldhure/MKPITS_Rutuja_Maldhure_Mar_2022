f1e39be21f18ef2953acc42c2953a286009f3091
rutuja maldhure <rutujamaldhure@gmail.com>
2022-05-12 23:14:53 +0530

    multiplication of matrix

--- C Programs/113_multiplication_of_matrix.c
+++ C Programs/113_multiplication_of_matrix.c
@@ -1,13 +1,14 @@
+//matrix multiplication
 #include<stdio.h>
 void main()
 {
-    int a[2][2], b[2][2], res[2][2], sum=0, i, j, k;
+    int a[3][3], b[3][3], res[3][3], sum=0, i, j, k;
     system("cls");
     
     printf("Enter elements of 1st matrix \n");
-    for(i=0; i<2; i++)
+    for(i=0; i<3; i++)
     {
-        for(j=0; j<2; j++)
+        for(j=0; j<3; j++)
         {
             printf("a[%d][%d]= ",i,j);
             scanf("%d",&a[i][j]);
@@ -15,22 +16,22 @@ void main()
     }
 
     printf("Enter elements of 2nd matrix \n");
-    for(i=0; i<2; i++)
+    for(i=0; i<3; i++)
     {
-        for(j=0; j<2; j++)
+        for(j=0; j<3; j++)
         {
             printf("b[%d][%d]= ",i,j);
             scanf("%d",&b[i][j]);
         }
     }
 
-    for(i=0; i<2; i++)
+    for(i=0; i<3; i++)
     {
-        for(j=0; j<2; j++)
+        for(j=0; j<3; j++)
         {
-            for(k=0; k<2; k++)
+            for(k=0; k<3; k++)
             {
-                sum=sum+(a[i][j]*b[k][j]);
+                sum=sum+(a[i][k]*b[k][j]);
             }
             res[i][j]=sum;
             sum=0;
@@ -38,13 +39,11 @@ void main()
     }
 
     printf("Matrix after Multiplication \n");
-    for(i=0; i<2; i++)
+    for(i=0; i<3; i++)
     {
-        for(j=0; j<2; j++)
+        for(j=0; j<3; j++)
         {
             printf("%d ",res[i][j]);
-            if(j==1)
-            printf("\n");
         }
     }
 }
