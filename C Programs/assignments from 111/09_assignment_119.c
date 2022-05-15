d095ce17dd03911c4c5493a17791c40bf422aba4
rutuja maldhure <rutujamaldhure@gmail.com>
2022-05-15 17:46:32 +0530

    sum of all array elements except 17 assignment-119

---
+++ C Programs/assignments from 111/09_assignment_119.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a[50],n,i,sum=0;
+    system("cls");
+    printf("Enter lenght of an array: ");
+    scanf("%d",&n);
+    printf("Enter elements of an array:\n");
+    for(i=0; i<n; i++)
+    {
+        printf("a[%d]= ",i);
+        scanf("%d",&a[i]);
+        if(a[i]!=17)
+        sum=sum+a[i];
+    }
+    printf("Sum of all values expect 17= %d",sum);
+}
