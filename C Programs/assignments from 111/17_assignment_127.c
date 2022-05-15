8cb27c70f00fc1fa74794e11221fecb21fa306b9
rutuja maldhure <rutujamaldhure@gmail.com>
2022-05-15 23:29:21 +0530

    check if two 5's nxt to each other or separated by one assignment-127

---
+++ C Programs/assignments from 111/17_assignment_127.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+    int a[50], n, i,c=0;
+    system("cls");
+    printf("Enter length of an array: ");
+    scanf("%d",&n);
+    printf("Enter elements of an array:\n");
+    for(i=0; i<n; i++)
+    {
+        printf("a[%d]= ",i);
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0; i<n; i++)
+    {
+        if(a[i]==5)
+        {
+            if(a[i+1]==5 || a[i+2]==5)
+            {
+                c=c+1;
+            }
+        }
+    }
+
+    if(c>0)
+    printf("True");
+    else
+    printf("False");
+}
