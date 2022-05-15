3ad01338753594b75592a15f4c743e676a2584aa
rutuja maldhure <rutujamaldhure@gmail.com>
2022-05-15 23:19:07 +0530

    check if 3 nxt to 3 or 5 nxt to 5 is present assignment-126

---
+++ C Programs/assignments from 111/16_assignment_126.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+    int a[50], n, i,c=0, b=0;
+    system("cls");
+    printf("Enter length of an array: ");
+    scanf("%d",&n);
+    printf("Enter elements of an array:\n");
+    for(i=0; i<n; i++)
+    {
+        printf("a[%d]= ",i);
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0; i<n; i++)
+    {
+        if(a[i]==3 && a[i+1]==3)
+        b=b+1;
+        if(a[i]==5 && a[i+1]==5)
+        c=c+1;
+    }
+    if(b>0 && c==0)
+    printf("3 next to 3 present");
+    if(b==0 && c>0)
+    printf("5 next to 5 is present");
+    if(b==0 && c==0)
+    printf("both not present");
+    if(b>0 && c>0)
+    printf("both are present i,e. 3 next to 3 and 5 next to 5 ");
+}
