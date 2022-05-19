afe9fa256a19a688224fc99d20bd390516774712
rutuja maldhure <rutujamaldhure@gmail.com>
2022-05-19 15:02:27 +0530

    create new array and shift all zeros towards left assi-137

---
+++ C Programs/assignments from 111/27_assignment_137.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+void main()
+{
+    int a[50],b[50],n,i,j,count=0;
+    system("cls");
+    printf("Enter length of an array: ");
+    scanf("%d",&n);
+    printf("Enter elements of an array:\n");
+    for(i=0; i<n; i++)
+    {
+        printf("a[%d]= ",i);
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0; i<n; i++)
+    {
+        if(a[i]==0)
+        count=count+1;
+    }
+
+    j=0;
+    for(i=0; i<n; i++)
+    {
+        if( a[i]==0 )
+        {
+            b[j]=0;
+            j++;
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        if(a[i]!=0)
+        {
+            b[j]=a[i];
+            j++;
+        }
+    }
+
+    printf("new array: ");
+    for(i=0; i<n; i++)
+    {
+        printf("%d ",b[i]);
+    }
+
+}
