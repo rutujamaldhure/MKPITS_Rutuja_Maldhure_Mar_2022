75448a96eae3f3ff566e21cd44c35ad0061a15fb
rutuja maldhure <rutujamaldhure@gmail.com>
2022-05-22 11:36:51 +0530

    print pattern

---
+++ C Programs/assignment to print pattern/26.c
@@ -0,0 +1,34 @@
+/*
+4 4 4 4 4 4 4
+  3 3 3 3 3
+    2 2 2
+      1
+*/
+#include<stdio.h>
+void main()
+{
+    int i, j, sp, n=4;
+    system("cls");
+    for(i=4; i>=1; i--)
+    {
+        for(sp=1; sp<=41-i; sp++)
+        {
+            printf(" ");
+        }
+
+        for(j=4; j>=1; j--)
+        {
+            if(j<=i)
+            printf("%d ",n);
+            else
+            printf(" ");
+        }
+        for(j=1; j<=4; j++)
+        {
+            if(j<i)
+            printf("%d ",n);
+        }
+        n--;
+        printf("\n");
+    }
+}
